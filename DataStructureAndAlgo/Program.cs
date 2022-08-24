6919ff62a9d73c8ebd41e8f9d3fe2f148da97b6c
rogineceniza@outlook.com <rogineceniza@outlook.com>
2022-08-24 17:29:57 +0800

    class

--- DataStructureAndAlgo/Program.cs
+++ DataStructureAndAlgo/Program.cs
@@ -1,3 +1,45 @@
-﻿string firstName = "Sam", lastName = "Antha";
+﻿using System.Transactions;
 
-Console.WriteLine($"My name is {firstName} {lastName.ToUpper()}. I live in Arriendelle");
+public class Program
+{
+
+    class Info
+    {
+        private readonly int _id;
+        private readonly string _password;
+
+
+        public int Id { get; }
+        public string password { get; }
+        public string firstName { get; set; } = String.Empty;
+        public string lastName { get; set; } = String.Empty;
+
+        public Info(string password, int id)
+        {
+            _password = password;
+            _id = id;
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine("Enter First Name: ");
+            string fn = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Enter Last Name: ");
+            string ln = Console.ReadLine();
+            Console.Clear();
+
+            
+            Console.WriteLine($"Welcome {fn} {ln}! Hope you enjoy your stay.");
+        }
+
+
+
+        private static void Main(string[] args)
+        {
+            Info info = new("sdf", 123);
+            info.getInfo();
+        }
+    }
+}
