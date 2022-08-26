a6b5f6c86776dfa74ca6e97528197ff25cbcf335
rogineceniza@outlook.com <rogineceniza@outlook.com>
2022-08-26 19:52:46 +0800

    Iterating 2D arrays

--- DataStructureAndAlgo/Program.cs
+++ DataStructureAndAlgo/Program.cs
@@ -1,20 +1,44 @@
-﻿//arrays
-int[] mads;
-mads = new int[2];
+﻿
+using System.Numerics;
 
-int[] add = new int[2];
-add[0] = 234;
-add[1] = 6789;
-
-int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-Console.WriteLine(add[0]);
-
-int[] myArray = new int[] { 12, 34, 56, 78, 890, 45, 56 };
-
-foreach (int i in array) 
+int[,] number = new int[,]
 {
-    Console.WriteLine(i);
+    {0, 0,0},
+    { 1, 1, 1},
+    { 2, 2, 2},
+    { 3, 3, 3},
+    { 4, 4, 4},
+    { 5, 5, 5},
+    { 6, 6, 6},
+    { 7, 7 , 7},
+    { 8, 8, 8},
+    { 9, 9, 9},
+    { 10, 10, 10},
+    { 11, 11, 11},
+};
+
+
+
+
+//for (int i = 0; i < number.GetLength(0); i++)
+//{
+//    //Console.Write("Row " + i + ": ");
+
+//    for (int j = 0; j < number.GetLength(1); j++)
+//    {
+//        Console.Write(number[i, j] + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+for (int i = 0; i < number.GetLength(0); i++)
+{
+    Console.Write($"Row {i}: ");
+    for (int j = 0; j <number.GetLength(1); j++)
+    {
+        Console.Write(number[i,j] + " ");
+    }
+    Console.WriteLine();
 }
 
-    foreach (int j in myArray)
-    { Console.WriteLine(j); }
+    
